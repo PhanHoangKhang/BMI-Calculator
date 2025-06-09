361fce7878fdbd20daf3e07a21cb4f112868e55a
Phan Hoàng Khang <togaphan@gmail.com>
2025-06-09 22:43:11 +0700

    Update CheckResult.py

--- CheckResult.py
+++ CheckResult.py
@@ -15,21 +15,21 @@ class CheckResult:
             if self.bmi < 18.5:
                 exact = 18.5 * (self.height * self.height)
                 needed = exact - self.weight
-                print(f"Need to gain {float(needed)} kg to be balanced")
+                print(f"Need to gain {float(needed):.2f} kg to be balanced")
             elif self.bmi > 24.9:
                 exact = 24.9 * (self.height * self.height)
                 needed = self.weight - exact
-                print(f"Need to lose {float(needed)} kg to be balanced")
+                print(f"Need to lose {float(needed):.2f} kg to be balanced")
             else:
                 print("Balanced!")
         elif self.gender == "female":
             if self.bmi < 18.5:
                 exact = 18.5 * (self.height * self.height)
                 needed = exact - self.weight
-                print(f"Need to gain {float(needed)} kg to be balanced")
+                print(f"Need to gain {float(needed):.2f} kg to be balanced")
             elif self.bmi > 24.9:
                 exact = 24.9 * (self.height * self.height)
                 needed = self.weight - exact
-                print(f"Need to lose {float(needed)} kg to be balanced")
+                print(f"Need to lose {float(needed):.2f} kg to be balanced")
             else:
                 print("Balanced!")
